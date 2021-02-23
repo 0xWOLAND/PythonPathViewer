469036e7f8db4898b71fb695a2199421e4bd3fcb
bhargavannem <bhargav.annem@gmail.com>
2021-02-23 17:59:36 -0600

    basic table

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ import pygame
 from pygame.locals import *
 
 pygame.init()
+WHITE = pygame.Color(255,255,255)
+DISPLAYSURF = pygame.display.set_mode((1000,1000))
+for i in range(0, 750, 50):
+    for j in range(0, 500, 50):
+        pygame.draw.rect(DISPLAYSURF, WHITE, (i + 125, j + 250, 40, 40), 0)
+
+
 
 while True:
-    while True:
-    pygame.display.update()
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
