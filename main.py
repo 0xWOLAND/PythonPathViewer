a5ba798e104b2a22c0a3ab31239280e00f932517
bhargavannem <bhargav.annem@gmail.com>
2021-02-23 18:51:25 -0600

    changes

--- main.py
+++ main.py
@@ -1,18 +1,97 @@
 import pygame
 from pygame.locals import *
+import math
 
-pygame.init()
-WHITE = pygame.Color(255,255,255)
-DISPLAYSURF = pygame.display.set_mode((1000,1000))
-for i in range(0, 750, 50):
-    for j in range(0, 500, 50):
-        pygame.draw.rect(DISPLAYSURF, WHITE, (i + 125, j + 250, 40, 40), 0)
+WIDTH = 800
+HEIGHT = 1000
 
+WIN = pygame.display.set_mode((WIDTH, WIDTH))
+pygame.display.set_caption("Path Finding Algorithm Visualizer")
 
+# Colors
 
-while True:
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-    pygame.display.update()
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+ORANGE = (255,165,0)
+PURPLE = (128, 0, 128)
+TURQUOISE = (64,224,208)
+
+class Node: 
+    def __init__(self, row, col, width, total_rows):
+        self.row = row
+        self.col = col
+        self.x = row * width
+        self.y = col * width
+        self.color = WHITE
+        self.neighbors = []
+        self.width = width
+        self.total_rows = total_rows
+
+    def get_pos(self):
+        return self.row, self.col
+
+    def is_closed(self):
+        return self.color == RED
+    
+    def is_open(self):
+        return self.color == GREEN
+    
+    def is_barrier(self):
+        return self.color == BLACK
+
+    def is_start(self):
+        return self.color == ORANGE
+    
+    def is_end(self):
+        return self.color == TURQUOISE
+
+    def reset(self):
+        self.color == WHITE
+
+    def make_closed(self):
+        self.color = RED
+    
+    def make_open(self):
+        self.color = GREEN
+    
+    def make_barrier(self):
+        self.color = BLACK
+
+    def make_start(self):
+        self.color = ORANGE
+    
+    def make_end(self):
+        self.color = TURQUOISE
+    
+    def make_path(self):
+        self.color = PURPLE
+    
+    def draw(self, win):
+        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.width))
+
+    def update_neighbors(self, grid):
+        pass
+
+    def __lt__(self, other):
+        return False
+
+def dist(p1, p2):
+    return abs(x1 - x2) + abs(y1 - y2)
+
+def make_grid(rows, width):
+    grid = []
+    gap = width // rows
+    for i in range(rows):
+        grid.append([])
+        for j in range(rows):
+            spot = Spot(i, j, gap, rows)
+            grid[i].append(spot)
+    
+    return grid
+
+def draw_grid(win, rows, width):
+    gap = width // rows
